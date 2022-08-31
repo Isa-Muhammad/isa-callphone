1088caf0f8418e5c038dc5fe9a1246e8a71ab47c
Isa Muhammad <isamuhammadm.im@gmail.com>
2022-08-31 21:56:02 +0100

    Feat: Complete dashboard page & logout

--- src/pages/app/Dashboard.js
+++ src/pages/app/Dashboard.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Logo from "../../assets/logo.png";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="main">
+      <div className="p-10">
+        <img alt="airgate" src={Logo} />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
